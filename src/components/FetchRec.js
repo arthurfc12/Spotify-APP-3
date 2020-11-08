e580ec835bc637543728c506b131e372dab376c6
lucaskf1996 <lucaskf1996@gmail.com>
2020-11-07 22:25:26 -0300

    head fix

--- src/components/FetchRec.js
+++ src/components/FetchRec.js
@@ -27,33 +27,33 @@ class FetchRec extends React.Component {
         if(this.props.acousticness<0.15) {
             acoustic = "&min_acousticness=0&max_acousticness="+String(this.props.acousticness+0.15)
         } else if(this.props.acousticness>0.85) {
-            acoustic = "&min_acousticness=0"+String(this.props.acousticness-0.15)+"&max_acousticness=0"
+            acoustic = "&min_acousticness="+String(this.props.acousticness-0.15)+"&max_acousticness=0"
         } else {
-            acoustic = "&min_acousticness=0"+String(this.props.acousticness-0.15)+"&max_acousticness="+String(this.props.acousticness+0.15)
+            acoustic = "&min_acousticness="+String(this.props.acousticness-0.15)+"&max_acousticness="+String(this.props.acousticness+0.15)
         }
 
         if(this.props.energy<0.15) {
             energy = "&min_energy=0&max_energy="+String(this.props.energy+0.15)
         } else if(this.props.energy>0.85) {
-            energy = "&min_energy=0"+String(this.props.energy-0.15)+"&max_energy=0"
+            energy = "&min_energy="+String(this.props.energy-0.15)+"&max_energy=0"
         } else {
-            energy = "&min_energy=0"+String(this.props.energy-0.15)+"&max_energy="+String(this.props.energy+0.15)
+            energy = "&min_energy="+String(this.props.energy-0.15)+"&max_energy="+String(this.props.energy+0.15)
         }
 
         if(this.props.liveness<0.15) {
             liveness = "&min_liveness=0&max_liveness="+String(this.props.liveness+0.15)
         } else if(this.props.liveness>0.85) {
-            liveness = "&min_liveness=0"+String(this.props.liveness-0.15)+"&max_liveness=0"
+            liveness = "&min_liveness="+String(this.props.liveness-0.15)+"&max_liveness=0"
         } else {
-            liveness = "&min_liveness=0"+String(this.props.liveness-0.15)+"&max_liveness="+String(this.props.liveness+0.15)
+            liveness = "&min_liveness="+String(this.props.liveness-0.15)+"&max_liveness="+String(this.props.liveness+0.15)
         }
 
         if(this.props.danceability<0.15) {
             dance = "&min_danceability=0&max_danceability="+String(this.props.danceability+0.15)
         } else if(this.props.danceability>0.85) {
-            dance = "&min_danceability=0"+String(this.props.danceability-0.15)+"&max_danceability=0"
+            dance = "&min_danceability="+String(this.props.danceability-0.15)+"&max_danceability=0"
         } else {
-            dance = "&min_danceability=0"+String(this.props.danceability-0.15)+"&max_danceability="+String(this.props.danceability+0.15)
+            dance = "&min_danceability="+String(this.props.danceability-0.15)+"&max_danceability="+String(this.props.danceability+0.15)
         }
 
         fetch('https://api.spotify.com/v1/recommendations?seed_genres='+this.props.seedGenres+acoustic+energy+dance+liveness, requestOptions)
@@ -64,6 +64,7 @@ class FetchRec extends React.Component {
     render(){
         if(this.state.recommendations!=""){
             const recArray = this.state.recommendations.tracks
+            console.log(recArray)
             if(recArray.length!==0){
                 return(
                     <div>
