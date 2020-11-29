84d697e000fc80a8dd6df5eef1b2ad69824cc70d
arthurfc12 <arthur0912fc@gmail.com>
2020-11-28 23:40:03 -0300

    funcionalidade

--- src/components/GetPlaylist.js
+++ src/components/GetPlaylist.js
@@ -1,49 +1,98 @@
 import React from "react";
-import PlaylistFeatures from './PlaylistFeatures.js';
+import PlaylistFeatures from "./PlaylistFeatures.js";
+import axios from "axios";
 
-class GetPlaylist extends React.Component{
+class GetPlaylist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userID: "",
+      playlists: [],
+    };
+    //this.fetchPlaylist = this.fetchPlaylist.bind(this)
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            images : [],
-            names: [],
-            userID: ""
+  componentWillMount() {
+    const requestOptions = {
+      headers: {
+        Authorization: "Bearer " + this.props.token,
+      },
+    };
 
-            
-        }
-    }
+    //${this.state.userID}
+    //this.setState({userID: localStorage.getItem("UserID")})
 
-    componentDidMount(){
-        const requestOptions = {
-            method: "GET",
-            headers: {
-                "Authorization": "Bearer " + this.props.token
-            }
-        };
-        //${this.state.userID}
-        //this.setState({userID: localStorage.getItem("UserID")})
-        const user = localStorage.getItem("UserID")
-        fetch(`https://api.spotify.com/v1/users/${user}/playlists`, requestOptions)
-            .then((response) => response.json())
-            .then((data) =>{
+    const user = localStorage.getItem("UserID");
 
-                console.log(this.state.userID)
-                console.log(data)
-            })
+    axios
+      .get(`https://api.spotify.com/v1/users/${user}/playlists`, requestOptions)
+      .then((response) => {
+        console.log(response.data.items);
+        //console.log(this.state.userID)
 
-        
-        
-        console.log(this.props.token)
-        
+        this.setState({ playlists: response.data.items }, () => {
+          console.log(this.state);
+        });
+      });
+  }
 
-    }
+  // fetchPlaylist(){
+  //     const pointToThis = this;
+  //     const user = localStorage.getItem("UserID")
+  //     let API_SPOTIFY = `https://api.spotify.com/v1/users/${user}/playlists`;
+  //     let namefetch = [];
+  //     let imagefetch = [];
 
-    render(){
-        return(<h1 className="titulos">Playlists</h1>)
-        
-    }
+  //     fetch (API_SPOTIFY)
+  //     .then(
+  //         function(response){
+  //             return response.json();
+  //         }
+  //     )
+  //     .then(
+  //         function(data){
+  //             // for(var key in data['Object'][key]['items']){
+  //                 // namefetch.push(data['Object'][key]['items']['name']);
+  //                 // imagefetch.push(data['Object'][key]['items']['images']['url']);
 
+  //         // }
 
+  //         var listagem = data.items;
+  //         listagem.map((music)=>{
+  //             console.log(music.name)
+  //             console.log(music.images[0].url)
+  //         })
+
+  //         //console.log(imagefetch)
+  //         //console.log(namefetch)
+
+  //         pointToThis.setState({
+  //             names: namefetch,
+  //             images: imagefetch
+  //         });
+  //     }
+  //   )
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1 className="titulos">Playlists</h1>
+        <ul>
+          {this.state.playlists.map((music) => {
+            return (
+              <li>
+                <p className="titulos">{music.name}</p>
+                <img src={music.images[0].url}></img>
+              </li>
+              //console.log('name',music.name)
+              //console.log('image',music.images[0].url)
+              //console.log(music)
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 export default GetPlaylist;
